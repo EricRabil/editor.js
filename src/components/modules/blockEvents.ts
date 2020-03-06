57fd5582c84f33338a6cce37ad8756b9888df965
Eric Rabil <ericjrabil@gmail.com>
2020-03-06 01:53:47 -0500

    ok here we go again babe

--- src/components/modules/blockEvents.ts
+++ src/components/modules/blockEvents.ts
@@ -342,9 +342,13 @@ export default class BlockEvents extends Module {
 
       const index = BlockManager.currentBlockIndex;
 
-      if (BlockManager.previousBlock && currentBlock.name === 'paragraph' && BlockManager.previousBlock.inputs.length === 0) {
+      if (BlockManager.previousBlock && BlockManager.previousBlock.inputs.length === 0) {
         /** If previous block doesn't contain inputs, remove it */
-        BlockManager.removeBlock(index - 1);
+        if (currentBlock.name === 'paragraph') {
+          BlockManager.removeBlock(index - 1);
+        } else {
+          BlockManager.replace('paragraph', {});
+        }
       } else {
         /** If block is empty, just remove it */
         BlockManager.removeBlock();
